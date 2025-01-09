17741ff51df591c6840cfe6e42de236089e606ba
Reed Monkey <shyaminreallife@gmail.com>
2025-01-09 12:08:40 +0545

    test

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,15 +1,3 @@
-const http= require("http")
-const { hostname } = require("os")
+const harry = require('./second');
 
-const hoatname="127.0.0.1"
-const port = 8000
-
-const server = http.createServer((req, res) =>{
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello world')
-})
-
-server.listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}`)
-})
+console.log("Hello world", harry);
---
+++ nodejs/second.js
@@ -0,0 +1,7 @@
+const harry = {
+    name: "harry",
+    favNum: 3,
+    developer: true
+};
+
+module.exports = harry;
