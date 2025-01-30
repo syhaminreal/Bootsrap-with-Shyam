85d78f0a7cf333be6cfb47f4603999fa49e0bab3
Reed Monkey <shyaminreallife@gmail.com>
2025-01-30 13:22:24 +0545

    hw

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
-alert("Hello ");
-alert("world");
+// Javascript   synchronus or single  thread
+
+// execution one line of the code at a TimeRanges
+// 
+// console.log(21)
+// console.log(13)
+
+
+//blocking code  maning one is waiting for the exection of the program  i.e reading the files.
+// non blocking  code one is not waiting for the completion of the code to be finished 
+
+
