7f51c9e834ddbe71dc533d9bb30328e999d1d538
syhaminreal <shyaminreallife@gmail.com>
2025-12-09 18:29:36 +0545

    goinng with promises

--- js/promises.js
+++ js/promises.js
@@ -1,17 +1,20 @@
-// fetch("https://something .com").then().catch.finally()
-// how ot make promises
+// // fetch("https://something .com").then().catch.finally()
+// // how ot make promises
 
-// promise 1 is an obect repesenting the completion of the task
-//creation plus consumption
+// // promise 1 is an obect repesenting the completion of the task
+// //creation plus consumption
 
-const promiseOne = new Promise(function (resolve, reject) {
-    // Simulate an async task (e.g., database call, network request)
-    setTimeout(function () {
-        console.log("Async task is completed");
-        resolve(); // Corrected spelling from 'resolove' to 'resolve'
-    }, 1000);
-});
+// const promiseOne = new Promise(function (resolve, reject) {
+//     // Simulate an async task (e.g., database call, network request)
+//     setTimeout(function () {
+//         console.log("Async task is completed");
+//         resolve(); // Corrected spelling from 'resolove' to 'resolve'
+//     }, 1000);
+// });
 
-promiseOne.then(function () {
-    console.log("Promise Consumed");
-});
+// promiseOne.then(function () {
+//     console.log("Promise Consumed");
+// });
+
+
+// // promise two
